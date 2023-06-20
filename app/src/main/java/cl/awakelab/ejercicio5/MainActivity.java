2715bcad2593227ebbff33b565fac6f53c82e5b9
Fabián <fanavar@alumnos.ubiobio.cl>
2023-06-19 21:12:18 -0400

    ejercicio 5 completado

--- app/src/main/java/cl/awakelab/ejercicio5/MainActivity.java
+++ app/src/main/java/cl/awakelab/ejercicio5/MainActivity.java
@@ -28,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(),"hello toast",Toast.LENGTH_SHORT).show();
             }
         });
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer numero = Integer.parseInt(textView.getText().toString());
+                numero++;
+                textView.setText(numero.toString());
+            }
+        });
     }
 
 }
